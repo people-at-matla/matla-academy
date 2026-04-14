fa6f407384a274841b45dad9d6ba9166cc19cc20
PAM <nkululeko.gumata@gmail.com>
2026-04-14 14:15:53 +0200

    Delete Matla Academy directory

--- Matla Academy/_fix_btt.py
+++
@@ -1,7 +0,0 @@
-css = """
-/* Move back-to-top above the floating chat FAB */
-.back-to-top { bottom: 5.2rem; right: 1.5rem; }
-"""
-with open('styles.css', 'a', encoding='utf-8') as f:
-    f.write(css)
-print('Done.')
